58679a83bceb88f50b5e9f9acf5f4c1e2b050294
Golov Roman <neyguvj@yandex.ru>
2026-03-20 16:36:13 +0300

    Test model temperature

--- client/main.py
+++ client/main.py
@@ -11,76 +11,56 @@ from langchain_core.prompts import ChatPromptTemplate
 load_dotenv()
 
 
+DEFAULT_MODEL = "ai-sage/GigaChat3-10B-A1.8B"
+DEFAULT_TEMPERATURE = 0.1
+
+
 BASE_PROMPT = """
-Ты ассистент для обучения программированию
-"""
-
-STEP_BY_STEP_PROMPT = (
-    BASE_PROMPT
-    + """
-Нужно решить задачу пошагово.
-"""
-)
-
-EXPERT_PROMPT = """
-1. Составь пошаговый алгоритм решения задачи
-2. Составь решение на основе полученного алгоритма
-3. Проанализируй полученный код на проблемы с производительностью
-4. Исправь код на основе полученных замечаний
+Ты дизайнер одежды для животных
 """
 
 
 TASK = """
-Дана шахматная доска 5 на 5
-Нужно обойти всю шахматную доску, поситив каждую клетку только один раз.
-Нужно посчитать суммарное количество всех маршрутов коня из каждой клетки шахматной доски.
+Придумай три вида зимней одежды для котов
 """
 
 
-GENERATE_PROMPT = (
-    TASK
-    + """
-Составь план, в котором будет постановка задачи и пошаговое описание алгоритма на естественном языке.
-"""
-)
-
-
-async def main():
+def init_model(model=DEFAULT_MODEL, temperature=DEFAULT_TEMPERATURE):
     api_key = os.environ.get("API_KEY")
     url = os.environ.get("BASE_URL")
     llm = ChatOpenAI(
         api_key=api_key,
         base_url=url,
-        model="ai-sage/GigaChat3-10B-A1.8B",
-        temperature=0.1,
+        model=model,
+        temperature=temperature,
     )
+    return llm
 
-    print("решение без подсказок")
+
+async def main():
+    print("temperature: 0")
+    print("prompt:", BASE_PROMPT)
     print("query:", TASK)
+    llm = init_model(temperature=0)
     response = await run(llm, BASE_PROMPT, TASK)
     time.sleep(10)
     print("\n" * 10)
 
-    print("пошаговое решение")
-    print("prompt:", STEP_BY_STEP_PROMPT)
-    print("query:", TASK)
-    response = await run(llm, STEP_BY_STEP_PROMPT, TASK)
-    time.sleep(10)
-    print("\n" * 10)
-
-    print("решение с генерацией промпта")
+    print("temperature: 0.05")
     print("prompt:", BASE_PROMPT)
-    print("query:", GENERATE_PROMPT)
-    generated_prompt = await run(llm, BASE_PROMPT, GENERATE_PROMPT)
-    print("generated_prompt:", generated_prompt)
-    response = await run(llm, generated_prompt, TASK)
+    print("query:", TASK)
+    llm = init_model(temperature=0.5)
+    response = await run(llm, BASE_PROMPT, TASK)
     time.sleep(10)
     print("\n" * 10)
 
-    print("решение от совета экспертов")
-    print("prompt:", EXPERT_PROMPT)
+    print("temperature: 1")
+    print("prompt:", BASE_PROMPT)
     print("query:", TASK)
-    response = await run(llm, EXPERT_PROMPT, TASK)
+    llm = init_model(temperature=1)
+    response = await run(llm, BASE_PROMPT, TASK)
+    time.sleep(10)
+    print("\n" * 10)
 
 
 async def run(llm, prompt, query):
